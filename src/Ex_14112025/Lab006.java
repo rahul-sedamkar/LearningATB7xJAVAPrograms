91a74c076727ad74c0eb9684c0fc2b20793b2a17
rahul-sedamkar <rahulsedamkar123@gmail.com>
2025-11-14 21:10:36 +0530

    All files commit

---
+++ src/Ex_14112025/Lab006.java
@@ -0,0 +1,34 @@
+package Ex_14112025;
+
+public class Lab006 {
+    public static void main(String[] args) {
+        //Data types
+
+        //Primitive data types
+            //Boolean
+                //True or False
+                boolean is_married = true;
+                boolean going_to_goa = false;
+                System.out.println(is_married);
+                System.out.println(going_to_goa);
+            //Numerical
+                //byte
+                byte b = 3;
+                //char
+                char c= 'B';
+                //short
+                short s= 34;
+                //int
+                int i= 56;
+                //long
+                long l = 678l;
+                //float
+                float f= 34.12f;
+                //double
+                double d= 23.676366;
+        //Non primitive data types
+                //string
+                String str = "Rahul";
+                String str1 = "H";
+    }
+}
